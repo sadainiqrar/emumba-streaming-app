86e943d918595a038b5ceab4ce05f56d7a9c4f66
sadain-iqrar-aera <sadain.iqrar@aeratechnology.com>
2024-12-23 16:42:04 +0500

    Initial commit

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  displayName: 'emumba-streaming-app',
+  preset: './jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: './coverage/emumba-streaming-app',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
